720d32a5b82ab3c18e936f73d3ac0d704275a44d
Fritchou2 <mohamedaziz.ferchichi@esprit.tn>
2022-03-02 01:05:36 +0100

    last thing

--- src/black_ops/test/Gestion_Competition/Joueur_Test.java
+++ src/black_ops/test/Gestion_Competition/Joueur_Test.java
@@ -5,7 +5,7 @@
  */
 package black_ops.test.Gestion_Competition;
 
-import black_ops.Controller.JoueurController;
+import black_ops.Controller.Joueur_Controller;
 import black_ops.Entity.Joueur;
 import black_ops.config.MaConnexion;
 
@@ -21,13 +21,14 @@ public class Joueur_Test {
     public static void main(String[] args) {
                    MaConnexion db = MaConnexion.getInstance();
 
-         JoueurController JoueurC = new JoueurController() ;
+         Joueur_Controller JoueurC = new Joueur_Controller() ;
         Joueur J1 = new Joueur(4,"Aziz","Gold","Fritchou",6) ;
         Joueur J2 = new Joueur (2,"Mokhtar","Gold","Valnloya",6) ;
         //JoueurC.ajouterJoueur(J1);//add
-        System.out.println(JoueurC.afficherJoueur());
+//        System.out.println(JoueurC.afficherJoueur());
         //JoueurC.UpdateJoueur(J1); // update
         //JoueurC.DeleteJoueur(J1);//Delete
+        System.out.println(JoueurC.RechercheAV("F"));
     }
     
 }
